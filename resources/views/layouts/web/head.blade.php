425c7faaef61da0f28f1826be2e8a18b567291d0
Shahriar201 <shahriarbrine201@gmail.com>
2020-02-14 17:10:15 +0600

    Split Template

---
+++ resources/views/layouts/web/head.blade.php
@@ -0,0 +1,23 @@
+<title>Travalers &mdash; Travelling Guideline</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700,900|Display+Playfair:200,300,400,700"> 
+    <link rel="stylesheet" href="{{ asset('asset/frontend/fonts/icomoon/style.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('asset/frontend/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('asset/frontend/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('asset/frontend/css/jquery-ui.css') }}">
+    <link rel="stylesheet" href="{{ asset('asset/frontend/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('asset/frontend/css/owl.theme.default.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('asset/frontend/css/bootstrap-datepicker.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('asset/frontend/fonts/flaticon/font/flaticon.css') }}">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mediaelement@4.2.7/build/mediaelementplayer.min.css">
+
+
+    <link rel="stylesheet" href="{{ asset('asset/frontend/css/aos.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('asset/frontend/css/style.css') }}">
